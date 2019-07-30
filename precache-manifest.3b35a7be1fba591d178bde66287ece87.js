6c8af367e1c19bf6c4c869d0aefbc106911aa69c
Vasiliy Lazarev <vasiliy.lazarev@ibm.com>
2019-07-30 09:44:53 +0300

    Updates

---
+++ precache-manifest.3b35a7be1fba591d178bde66287ece87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7e39ae8071fa204fb65de07a082bc8",
+    "url": "/react-flip-card/index.html"
+  },
+  {
+    "revision": "13a5ef14610631dfe8ab",
+    "url": "/react-flip-card/static/css/main.a3930577.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-flip-card/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "13a5ef14610631dfe8ab",
+    "url": "/react-flip-card/static/js/main.3b73b804.chunk.js"
+  },
+  {
+    "revision": "da832b12a0c136ee3c32",
+    "url": "/react-flip-card/static/js/runtime~main.7ddfda00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3091a2e4a1f0cf8f2dc7d3d2adcd88b1.js"
+  "/react-flip-card/precache-manifest.3b35a7be1fba591d178bde66287ece87.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-flip-card/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
